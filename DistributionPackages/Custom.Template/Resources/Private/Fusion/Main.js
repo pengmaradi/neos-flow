c7815cd433125cffbf49e4e6c919e3ead10e06b2
Xiaoling Peng <xpeng@dachcom.ch>
2022-12-11 18:24:43 +0100

    chore: changed some fusions and  installed Custom.Team

--- DistributionPackages/Custom.Template/Resources/Private/Fusion/Main.js
+++ DistributionPackages/Custom.Template/Resources/Private/Fusion/Main.js
@@ -1,5 +1,13 @@
+'use strict';
+
 //import "tw-elements";
 
 // console.log('try');
 // debugger;
 // console.log('next try');
+
+import Team from '../JavaScript/team';
+
+document.addEventListener('DOMContentLoaded', () => {
+    new Team();
+});
