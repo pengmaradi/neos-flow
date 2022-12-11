0fef56d37c94a85ad6065c6e3a4b310ec5e9edd3
Xiaoling Peng <xpeng@dachcom.ch>
2022-12-11 18:22:37 +0100

    feat: added Custom.Team

---
+++ DistributionPackages/Custom.Template/Resources/Private/JavaScript/team.js
@@ -0,0 +1,22 @@
+'use strict';
+
+export default class Team {
+
+    constructor() {
+        const selectElement = document.querySelector('.personform__select');
+
+        selectElement.addEventListener('change', (event) => {
+            let catId = event.target.value;
+            const persionElement = document.querySelectorAll('.custom_team_content_persioncart');
+
+            persionElement.forEach((element, index) => {
+
+                if (!element.querySelector('.persioncart').classList.contains(catId) && catId.length > 0) {
+                    element.classList.add('hidden');
+                } else {
+                    element.classList.remove('hidden');
+                }
+            });
+        });
+    }
+}
