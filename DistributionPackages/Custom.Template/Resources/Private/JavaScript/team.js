ea4e9a139ad767b448d5aa2f861f967cf21f62f9
Xiaoling Peng <xpeng@dachcom.ch>
2022-12-13 07:59:43 +0100

    chore: created team search js

--- DistributionPackages/Custom.Template/Resources/Private/JavaScript/team.js
+++ DistributionPackages/Custom.Template/Resources/Private/JavaScript/team.js
@@ -4,19 +4,20 @@ export default class Team {
 
     constructor() {
         const selectElement = document.querySelector('.personform__select');
+        if (selectElement !== null) {
+            selectElement.addEventListener('change', (event) => {
+                let catId = event.target.value;
+                const persionElement = document.querySelectorAll('.custom_team_content_persioncart');
 
-        selectElement.addEventListener('change', (event) => {
-            let catId = event.target.value;
-            const persionElement = document.querySelectorAll('.custom_team_content_persioncart');
+                persionElement.forEach((element, index) => {
 
-            persionElement.forEach((element, index) => {
-
-                if (!element.querySelector('.persioncart').classList.contains(catId) && catId.length > 0) {
-                    element.classList.add('hidden');
-                } else {
-                    element.classList.remove('hidden');
-                }
+                    if (!element.querySelector('.persioncart').classList.contains(catId) && catId.length > 0) {
+                        element.classList.add('hidden');
+                    } else {
+                        element.classList.remove('hidden');
+                    }
+                });
             });
-        });
+        }
     }
 }
