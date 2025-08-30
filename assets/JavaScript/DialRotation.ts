3ec8fe3d9bda23f18d3722ad5bfbc0f339efa6eb
Xiaoling Peng <pengmaradi@gmail.com>
2025-08-30 15:45:51 +0200

    [TEST] try dial rotation with style

--- assets/JavaScript/DialRotation.ts
+++ assets/JavaScript/DialRotation.ts
@@ -1,10 +1,12 @@
 import Alpine from 'alpinejs'
 import { gsap } from 'gsap'
-import {Draggable} from 'gsap/Draggable'
+import { Draggable } from 'gsap/Draggable'
 
 const DialRotation = () => {
     Alpine.data('dialRotation', () => ({
         imageURLs: [] as string[],
+        draggableInstance: null as any,
+        spin: null as gsap.core.Timeline | null,
         init() {
             gsap.registerPlugin(Draggable)
             this.$nextTick(() => {
@@ -12,54 +14,64 @@ const DialRotation = () => {
                 this.imageURLs = str ? str.split(',') : []
                 const circle = this.$refs.mainCircle
                 const images = this.placeImages(this.imageURLs, circle)
-                const spin = gsap.timeline({repeat: -1, defaults: { duration: 50, ease: "none"}})
-                    .to(circle, {rotation: 360})
-                    .to(images, {rotation: -360}, 0)
 
-                this.drapable(circle, spin)
+                images.forEach((item) => {
+
+                    item.addEventListener('click', () => {
+                        images.forEach(img => {
+                            img.style.zIndex = '0';
+                        })
+                        item.style.zIndex = '1'
+                    })
+                })
+
+                this.spin = gsap.timeline({
+                        repeat: -1, 
+                        defaults: { duration: 40, ease: "none" }
+                    })
+                    .to(circle, { rotation: 360 })
+                    .to(images, { rotation: -360 }, 0)
+                
+                Draggable.create(circle, {
+                    type: 'rotation',
+                    inertia: true,
+                    onPressInit: () => {
+                        this.spin?.pause()
+                    },
+
+                    onDrag: () => {
+                        let rotation = gsap.getProperty(circle, "rotation") as number
+                        let angle = (rotation + 360) % 360
+                        this.spin?.progress(angle/360).play()
+                    }
+
+                })
+                
             })
         },
         placeImages(imageURLs: string[], circle: HTMLElement) {
-            let angleIncrement = Math.PI * 2 / imageURLs.length,
-                radius = circle.offsetWidth / 2,
+            let angleIncrement: number = Math.PI * 2 / imageURLs.length,
+                radius: number = circle.offsetWidth / 2,
                 images = [],
-                image, angle, i
-            for (i = 0; i < imageURLs.length; i ++) {
+                image, angle: number
+            
+            for (let i = 0; i < imageURLs.length; i++) {
                 image = new Image();
                 images.push(image);
                 circle.appendChild(image);
                 angle = angleIncrement * i;
+                
                 gsap.set(image, {
-                    attr:{ src: imageURLs[i] },
-                    position:"absolute", 
-                    top:0, 
-                    left:0, 
-                    xPercent:-50, 
-                    yPercent:-50,
-                    transformOrigin:"50% 50%",
+                    attr: { src: imageURLs[i] },
+                    xPercent: -50,
+                    yPercent: -50,
+                    transformOrigin: "50% 50%",
                     x: radius + Math.cos(angle) * radius,
-                    y: radius + Math.sin(angle) * radius
+                    y: radius + Math.sin(angle) * radius,
                 });
             }
             return images;
         },
-
-        drapable(circle: HTMLElement, spin: any) {
-            Draggable.create(circle, {
-                type: 'rotation',
-                inertia: true,
-                onPressInit: () => spin.pause(),
-                onDrag: () => {
-                    let angle = gsap.getProperty(circle, "rotation") as number
-                    spin.progress((angle % 360) / 360)
-                },
-                onThrowComplete: () => {
-                    spin.resume()
-                    gsap.fromTo(spin, {timeScale: 0}, {duration: 1, timeScale: 1, ease: 'power1.in'})
-                }
-            })
-        },
-
     }))
 }
 
